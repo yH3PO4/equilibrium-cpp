25b77a091b751677979406ae49beba676c2ef0a8
yH3PO4 <55231468+yH3PO4@users.noreply.github.com>
2023-05-20 09:36:18 +0900

    0519

--- src/network.hpp
+++ src/network.hpp
@@ -6,8 +6,7 @@
 struct Node
 {
     int id;
-    double lon;
-    double lat;
+    double lon, lat;
     Node(int _nodeID, double _lat, double _lon);
 };
 
@@ -19,19 +18,26 @@ enum class LinkType
 
 struct Link
 {
-    int id;
-    int oNodeID;
-    int dNodeID;
-    int laneCount;
-    int maxSpeed;
+    int id, oNodeID, dNodeID, laneCount, maxSpeed;
     LinkType type;
+    double flow, newflow, cost, freecost, capacity;
     Link(int _id, int _oNodeID, int _dNodeID, int _laneCount, int _maxSpeed);
 };
 
-struct Graph
+class Graph
 {
+public:
     std::unordered_map<int, Node> nodes;
     // directed graph
     std::unordered_map<int, std::vector<Link>> adj_list;
     Graph(std::vector<Node> nodes, std::vector<Link> links);
+    void assignment(double &alpha, double &beta);
+
+private:
 };
+
+double bpr(const double &flow,
+           const double &freecost,
+           const double &capacity,
+           const double &alpha,
+           const double &beta);
