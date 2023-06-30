4c8400ad68a43290cefe027e2ecf09b5fa5994cc
yH3PO4 <55231468+yH3PO4@users.noreply.github.com>
2023-06-30 07:01:29 +0000

    コンパイルが通るようにする

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -3,6 +3,9 @@
 #include <boost/geometry/geometries/point_xy.hpp>
 #include <boost/geometry/index/rtree.hpp>
 #include <boost/graph/adjacency_list.hpp>
+// https://stackoverflow.com/questions/46156995/boost-1-65-1-geometry-distance-strategy-compile-error-with-visual-studio-2017
+#include <boost/geometry/algorithms/comparable_distance.hpp>
+#include <boost/geometry/strategies/strategies.hpp>
 
 namespace bg = boost::geometry;
 namespace bgi = boost::geometry::index;
